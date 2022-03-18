306ffe602046165b46270c1eb94d9cc357f58bf2
jiax <sossolee@163.com>
2022-03-18 16:32:12 +0800

    1st day

---
+++ learn.py
@@ -0,0 +1,28 @@
+
+
+print("hola")
+#wo xie lai play play
+'''
+BUZZ & WOODY are best friends
+'''
+if True:
+    print("1st")
+    print("BUZZ LIGHTYEAR")
+else:
+    print("answer")
+    print("false")
+item_one = 1
+item_two = 2
+item_three = 3
+total = item_one + \
+    item_two + \
+        item_three
+if total == 6:
+    print("WOODY")
+else:
+        print("hh")
+days = ['SUN','MON','TUE',
+'WED','THUR']
+print(days[2])
+
+input("按下ENTER退出，其他键显示。。。\n")
