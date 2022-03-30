29b1dd2402faff321b2fdfeca1832ee02880471f
jiax <sossolee@163.com>
2022-03-30 15:14:04 +0800

    x.py

--- x.py
+++ x.py
@@ -1,4 +1,5 @@
 #python条件控制
+from itertools import count
 import numbers
 
 
@@ -54,3 +55,51 @@ else:
         print("整除3")
     else:
         print("蒜了")
+
+'''
+3.30 python3循环语句
+'''
+#计算1-100总和
+n = 100
+
+sum = 0
+counter = 1
+while counter <= n:
+    sum = sum + counter
+    counter += 1
+print("1 到 %d 之和为 %d" % (n, sum))
+#无限循环
+var3 = 1
+while var3 == 1:
+    num = int(input("输入一个数字 ： "))
+    print("输入的数字是：", num)
+print("bye!!!")
+#while循环使用else语句
+#循环输出数字，判断大小
+count1 = 0
+while count1 < 5:
+    print(count1, "小于5")
+    count1 += 1
+else:
+    print(count1, "大于或等于 5")
+#简单语句组
+flag = 1
+while (flag): print("hola!")
+
+print("goodbye!")
+#for 语句
+#在for实例中使用了break语句
+sites = ["BD", "GG", "rb", "TB"]
+for site in sites:
+    if site == "rb":
+        print("菜鸟教程！")
+        break
+    print("循环数据" + site)
+else:
+    print("null")
+print("done!")
+#range()函数
+for i in range(5, 9):
+    print(i)
+for i in range(0, 10, 3):
+    print(i)
