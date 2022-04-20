90664cdad67db7540a331e61eecadab48fad117d
jiax <sossolee@163.com>
2022-04-20 11:16:48 +0800

    h.py

--- h.py
+++ h.py
@@ -1,5 +1,7 @@
 #排2min 10业务task 周期t为20ms 50ms 100ms 200ms 500ms 优先级lp
 #先排1s 2000 slot 100ms周期
+#同一slot里的同一时刻也是可以并发业务的，基站给到slot的资源，分配到终端，怎么使用是终端行为
+import split_list
 
 
 n = 2000
@@ -13,8 +15,7 @@ for slot in range(len(slots)):
         slots[slot] += 10*b
         print(slots)
         if slot <= 2000:
-            b += 1
-            
+            b += 1   
         else:
             break
         list1.append(slot)
@@ -45,7 +46,7 @@ nb = int(50 / 0.5)
 nc = int(100 / 0.5)
 nd = int(200 / 0.5)
 ne = int(500 / 0.5)
-print('五个任务的周期间隔slot数',na, nb, nc, nd, ne)
+print('五类任务的周期间隔slot数',na, nb, nc, nd, ne)
 #此时ne的间隔发送时间>2s
 #①终端和业务都占不满网络，发送数据只需要排列优先级，将数据在同一时隙发送
 #业务输入
@@ -57,19 +58,25 @@ print('业务与优先级输入：',tsklp1)
 #优先级排序
 #tsklp = [('a', 3), ('b', 1), ('c', 4), ('d', 2), ('e', 5)]
 tsklp1.sort(key=lambda x:x[1], reverse= True)
-print('可按照如下顺序在slot1发出：', tsklp1)
+print('未达到网络资源满载，按照如下顺序在slot1发出：', tsklp1)
+
 
 #②终端多于8个，但业务数据量不超过时隙负载
 #优先级排序
-tasks = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1','d','e','f','g', 'h', 'k']
-lps = [3, 1, 4, 2, 5,3, 1, 4, 2, 5,3, 1, 4]
+#最大承载终端数tps，优先级lps， 
+tps = 8
+tasks = ['a', 'b', 'c','d','e','f','g', 'h', 'i', 'j', 'k']
+lps = [3, 1, 4, 2, 5, 3, 1, 4, 2, 5, 3]
 tt = zip(tasks,lps)
 tsklp2 = list((task, lp) for task, lp in tt)
 tsklp2.sort(key=lambda x:x[1], reverse= True)
-print(tsklp2)
+#print(tsklp2)
+tsklist = split_list.list_of_groups(tsklp2,tps)
+print('slot1中发送任务：', tsklist[0], '\nslot2中发送任务：', tsklist[1])
+
 
 #③终端少于8，但数据超过时隙负载，一个时隙发一项业务（×）一个时隙发n个业务，可以并发
-#同一slot里的同一时刻也是可以并发业务的，基站给到slot的资源，分配到终端，怎么使用是终端行为
+
 
 
 
